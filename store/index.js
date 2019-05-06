5aaabfde700b51e6b1145b8661966fcfe5be5971
hazeyjane <nimbrg@gmail.com>
2019-05-06 21:11:58 +0300

    Add files via upload

--- store/index.js
+++ store/index.js
@@ -1,7 +1,12 @@
 import rootReducer from "./reducers";
 import { createStore, applyMiddleware, compose } from "redux";
+import { autoRehydrate } from 'redux-persist'
+
+// here we create our redux store
+// we use 'redux-persist' to save and load our redux store when the app 
+// after closing the app
 
 export function configureStore() {
-	const store = createStore(rootReducer);
+  const store = createStore(rootReducer, compose(autoRehydrate()));
 	return store;
 }
