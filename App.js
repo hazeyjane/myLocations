209253890f40c9aa4b4469dc1e0ad43fc927140c
hazeyjane <nimbrg@gmail.com>
2019-05-06 21:11:20 +0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,15 +1,24 @@
 import * as React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, AsyncStorage } from 'react-native';
 import { Constants } from 'expo';
 import { Provider } from 'react-redux';
+import { persistStore } from 'redux-persist';
 import { configureStore } from './store';
 import NavBar from './components/NavBar';
 
+// initiating and configuring our redux store
 const store = configureStore();
 
 export default class App extends React.Component {
+  componentWillMount() {
+    // the app state is being persisted using 'redux-persist'
+    persistStore(store, { storage: AsyncStorage });
+  }
+
   render() {
-   return (
+    // wrapping our app with a provider sp we can access the redux store
+    // from child components
+    return (
       <Provider store={store}>
         <View style={styles.container}>
           <NavBar />
@@ -22,9 +31,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',   
-    paddingTop: Constants.statusBarHeight, 
-    //backgroundColor: '#ecf0f1',
-    // padding: 8,
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
   },
 });
