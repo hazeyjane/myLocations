c674e62e4b922691df2b7f071bbf561b6338d8b3
hazeyjane <nimbrg@gmail.com>
2019-05-07 02:30:46 +0300

    Update LocationList.js

--- components/LocationList.js
+++ components/LocationList.js
@@ -95,8 +95,8 @@ class LocationList extends React.Component {
 		const uniqueCategories = Array.from(new Set(categories));
 		const sections = uniqueCategories.map(cat => {      
 			const data = this.props.locations.filter(item => item.category === cat)
-      const catItem = this.props.categories.find(item => item.key == cat)
-      const catName = cat == undefined ? '' : catItem.name;
+      const catItem = this.props.categories.find(item => item.key == cat)     
+      const catName = cat == undefined || catItem == undefined ? '' : catItem.name;
       return { title: catName, data: data }          		
 		});
 		return sections;
